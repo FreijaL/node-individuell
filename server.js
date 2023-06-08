38e1ffce0d28744a631d250e74d7543e3981d60b
FreijaL <110614499+FreijaL@users.noreply.github.com>
2023-06-08 22:35:46 +0200

    menu, add, update done

--- server.js
+++ server.js
@@ -1,47 +1,81 @@
 const express = require('express');
 const nedb = require('nedb-promise');
 const cors = require('cors');
-//import { nanoid } from require('nanoid');
-//const { accessControl } = require('./middleware/accessControl');
-//const fs = require('fs');
+const fs = require('fs');
 
-// Skriva ny fil - ej relevant nu - behövs för att lägga till saker i menyn
 const menuDB = new nedb({filename: 'menu.db', autoload: true});
 
 const app = express();
 const port = 6000;
 
-//const id = nanoid();
-
-
-
 app.use( express.json() );
 app.use( cors({ origin: '*' }) );
-// app.use( (req, res, next) => {
-//     console.log(req.method, req.url, res.body);
-//     next();
-// });
 
-menuDB.insert({ item: 'Pizza', price: 12 }, (err, newItem) => {
-    if (err) console.log('Error adding test data:', err);
-  });
 
 app.get('/api/menu', async (req, res) => {
-    try {
-        const menu = await menuDB.find({}).exec();
-        res.json(menu)
-    } catch (error) {
+    const menu = await menuDB.find({});
+    res.json(menu)
+});
+
+
+app.post('/api/addproduct', async (req, res) => {
+    const product = req.body;
+    const existingProduct = await menuDB.findOne({ name: req.body.name });
+
+    if( existingProduct ){
         res
-            .status(500)
-            .json( {error: "Items not found, check your connection."})
+            .status(409)
+            .send('Product already exist')
+    } else {
+        product.createdAt = new Date()
+        product.modifiedAt = new Date()
+        menuDB.insert(product);
+        res
+            .status(201)
+            .send('Product added');
     }
 });
 
- // let menu = require('./data/menu.db')
-        // res
-        //     .json( menu )
-        //     .status(200);
-//app.get('/api/admin', accessControl );
+
+app.put('/api/updateproduct', async (req, res) => {
+    const { id, whatToUpdate, updateTo } = req.body;
+    const product = req.body;
+
+    try {
+        if ( whatToUpdate == 'name' ){
+            await menuDB.update({ _id: id }, { $set: { name: updateTo, modifiedAt: new Date() } });
+        } else if ( whatToUpdate == 'price') {
+            await menuDB.update({ _id: id }, { $set: { price: updateTo, modifiedAt: new Date() } });
+        } else if ( whatToUpdate == 'description' ) {
+            await menuDB.update({ _id: id }, { $set: { description: updateTo, modifiedAt: new Date() } });
+        } else {
+            res
+                .status(406)
+                .send('Property does not exist, please use name/price/description/id')
+        }
+        res.send('Product updated')
+    } catch (error) {
+        console.log(error);
+        res
+            .status(409)
+            .send('Something went wrong, try again')
+    }
+});
+
+
+app.delete('/api/delete', async (req, res) => {
+    const productId = req.params.id;
+    await menuDB.remove({ _id: productId }, function (error, removed) {
+        if (error) {
+            console.log(error, 'No product found, please try a different id');
+        } else {
+            console.log(removed);
+        }
+    });
+    res.send('Product deleted')
+});
+
+
 
 // app.get('/api/admin/delete')
 // app.get('/api/admin/create')
